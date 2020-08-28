a0780254f9f44babcb3edb500a896a462afe891e
SuperGx <mitreageorge14@stud.ase.ro>
2020-08-28 15:07:11 +0300

    Add project files.

---
+++ wwwroot/js/index.js
@@ -0,0 +1,9 @@
+﻿console.log("JET FA!")
+
+var form = document.getElementById("theForm");
+form.hidden = true;
+
+var button = document.getElementById("buyButton");
+button.addEventListener("click", function () {
+    alert("Buying the item")
+});
